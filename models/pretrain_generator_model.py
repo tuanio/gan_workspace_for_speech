466c8722be18a6abb8159dfbb8b39cd017283767
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-21 13:08:22 +0800

    fix

--- models/pretrain_generator_model.py
+++ models/pretrain_generator_model.py
@@ -29,7 +29,7 @@ class PretrainGeneratorModel(BaseModel):
             self.criterion = torch.nn.L1Loss()
 
             self.optimizer = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer)
 
     def set_input(self, input):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
