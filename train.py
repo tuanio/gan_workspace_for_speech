5855802c09e5fb7089fa04d9fc83686092f94628
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-22 16:10:11 +0800

    change the step

--- train.py
+++ train.py
@@ -41,12 +41,12 @@ if __name__ == '__main__':
 
     step = 0
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
-        step += 1
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
 
         for i, data in enumerate(dataset):  # inner loop within one epoch
+            step += 1
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
